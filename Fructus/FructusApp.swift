213f658dd22444d8acde41dd332ec32d963f3a52
Jonathan Silva <jonathan.jgs@gmail.com>
2021-10-02 15:45:36 -0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Jonathan Goncalves da Silva on 02/10/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
